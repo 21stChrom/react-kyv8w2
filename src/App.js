d8c89b00de482155c15add1a624bff8064a832ac
21stChrom <parkerjmartin2020@gmail.com>
2022-06-26 11:27:48 -0400

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ export default function App() {
   return (
     <div class="container">
       <h1 className="chico">Hel-oTImE</h1>
+      
       <audio style={{backgroundColor:"blueviolet", borderRadius:"15%"}} className="chic" controls>
-        <source src="./juicewrld999" type="audio/mpeg" />
-        </audio><div className="chic"><div  className="Pmack">
+        <source src="" type="audio/mpeg" />
+        </audio>
+        
+        <div className="chic"><div  className="Pmack">
      <div style={{color: "red"}}><BsFillDropletFill /></div>
     <div className="Pmack"><span className="bibop" style={{color: "red"}}><BsFillDropletFill /></span></div>
     </div>
